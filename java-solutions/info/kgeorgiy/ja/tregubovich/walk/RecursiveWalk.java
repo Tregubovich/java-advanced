2b60027b2bc41177c8db3ed1d5f8cae94920966b
Tregubovich Andrei <326632andrey@gmail.com>
2026-02-13 14:16:37 +0300

    Homework 1

--- java-solutions/info/kgeorgiy/ja/tregubovich/walk/RecursiveWalk.java
+++ java-solutions/info/kgeorgiy/ja/tregubovich/walk/RecursiveWalk.java
@@ -8,12 +8,20 @@ import java.util.stream.Collectors;
 
 public class RecursiveWalk {
 
+    static boolean FNV_32;
+    static boolean RECURSIVE;
+
     public static void main(String[] args) {
-        if (args == null || args.length != 2) {
-            System.err.println("Usage: java RecursiveWalk <input> <output>");
+        if (args == null || args.length < 2) {
+            error("Usage: java RecursiveWalk <input> <output> [<hash-type>]");
             return;
         }
-        walk(args[0], args[1]);
+        if (args.length == 3 && args[2] != "fnv-32" && args[2] != "fnv-64") {
+            error("Invalid hash type");
+            return;
+        }
+        boolean fnv32 = args.length < 3 || args[2].equals("fnv-32");
+        walk(args[0], args[1], fnv32, true);
     }
 
     protected static Path makeInput(String input) {
@@ -61,6 +69,17 @@ public class RecursiveWalk {
     }
 
     public static void walk(String input, String output) {
+        walk(input, output, true, true);
+    }
+
+    public static void walk(
+        String input,
+        String output,
+        boolean fnv32,
+        boolean recursive
+    ) {
+        FNV_32 = fnv32;
+        RECURSIVE = recursive;
         Path inputPath;
         if ((inputPath = makeInput(input)) == null) {
             return;
@@ -108,24 +127,43 @@ public class RecursiveWalk {
     protected static void invalidFile(String path, Writer writer, String msg)
         throws IOException {
         error(msg);
-        writer.write(String.format("%08x", 0) + " " + path + "\n");
+        writer.write(
+            (FNV_32 ? String.format("%08x", 0) : String.format("%016x", 0L)) +
+                " " +
+                path +
+                "\n"
+        );
     }
 
     protected static void proceedLine(Path path, Writer writer)
         throws IOException {
         if (path.toFile().isFile()) {
-            int hash = hashOfFile(path);
+            long hash = hashOfFile(path);
             String res =
-                String.format("%08x", hash) + " " + path.toFile().toString();
+                (FNV_32
+                    ? String.format("%08x", (int) hash)
+                    : String.format("%016x", hash)) +
+                " " +
+                path.toFile().toString();
             writer.write(res + "\n");
-        } else if (path.toFile().isDirectory()) {
+        } else if (RECURSIVE && path.toFile().isDirectory()) {
             for (Path child : Files.list(path).collect(Collectors.toList())) {
                 proceedLine(child, writer);
             }
+        } else {
+            error(path.toFile().toString() + " is not a file");
+            writer.write(
+                (FNV_32
+                        ? String.format("%08x", 0)
+                        : String.format("%016x", 0L)) +
+                    " " +
+                    path.toFile().toString() +
+                    "\n"
+            );
         }
     }
 
-    protected static int hashOfFile(Path path) {
+    protected static long hashOfFile(Path path) {
         char[] buffer = new char[2048];
         try (
             BufferedReader reader = Files.newBufferedReader(
@@ -134,9 +172,17 @@ public class RecursiveWalk {
             );
         ) {
             int read;
-            int hval = 0x811c9dc5;
-            while ((read = reader.read(buffer)) != -1) {
-                hval = fnv(hval, buffer, read);
+            long hval;
+            if (FNV_32) {
+                hval = 0x811c9dc5;
+                while ((read = reader.read(buffer)) != -1) {
+                    hval = fnv32((int) hval, buffer, read);
+                }
+            } else {
+                hval = 0xcbf29ce484222325L;
+                while ((read = reader.read(buffer)) != -1) {
+                    hval = fnv64(hval, buffer, read);
+                }
             }
             return hval;
         } catch (IOException e) {
@@ -145,9 +191,19 @@ public class RecursiveWalk {
         }
     }
 
-    protected static int fnv(int x0, char[] buf, int read) {
+    protected static int fnv32(int x0, char[] buf, int read) {
         final int fnv_prime = 0x01000193;
         int hval = x0;
+        for (int i = 0; i < read; i++) {
+            hval *= fnv_prime;
+            hval ^= buf[i];
+        }
+        return hval;
+    }
+
+    protected static long fnv64(long x0, char[] buf, int read) {
+        final long fnv_prime = 0x00000100000001b3L;
+        long hval = x0;
         for (int i = 0; i < read; i++) {
             hval *= fnv_prime;
             hval ^= buf[i];
--- java-solutions/info/kgeorgiy/ja/tregubovich/walk/Walk.java
+++ java-solutions/info/kgeorgiy/ja/tregubovich/walk/Walk.java
@@ -7,11 +7,19 @@ import java.nio.file.*;
 public class Walk {
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.err.println("Usage: java Walk <input> <output>");
+        if (args == null || args.length < 2) {
+            error("Usage: java RecursiveWalk <input> <output> [<hash-type>]");
             return;
         }
+        if (args.length == 3 && args[2] != "fnv-32" && args[2] != "fnv-64") {
+            error("Invalid hash type");
+            return;
+        }
+        boolean fnv32 = args.length < 3 || args[2].equals("fnv-32");
+        RecursiveWalk.walk(args[0], args[1], fnv32, false);
+    }
 
-        RecursiveWalk.walk(args[0], args[1]);
+    private static void error(String message) {
+        System.err.println(message);
     }
 }
