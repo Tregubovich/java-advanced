2c30fc8d6b346606cb250856a0abb07e21b7f25d
Tregubovich Andrei <326632andrey@gmail.com>
2026-03-13 12:49:47 +0300

    Homework 5

--- java-solutions/info/kgeorgiy/ja/tregubovich/implementor/Implementor.java
+++ java-solutions/info/kgeorgiy/ja/tregubovich/implementor/Implementor.java
@@ -11,7 +11,7 @@ import java.lang.reflect.Modifier;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -19,59 +19,102 @@ import java.util.stream.Stream;
 public class Implementor implements Impler {
     @Override
     public void implement(Class<?> aClass, Path path) throws ImplerException {
-        if (aClass.isPrimitive() || aClass.equals(String.class) || aClass.equals(Enum.class)) {
+        if (aClass.isPrimitive()
+                || aClass.isArray()
+                || Modifier.isFinal(aClass.getModifiers())
+                || Modifier.isPrivate(aClass.getModifiers())
+                || aClass.equals(Enum.class)
+                || aClass.equals(Record.class)) {
             throw new ImplerException(aClass.getName() + " can't be implemented or extended");
         }
-        Path classPath = path.resolve(Arrays.stream(aClass.getName().split("\\."))
+        Path classPath = path.resolve(Arrays.stream(aClass.getPackageName().split("\\."))
                 .collect(Collectors.joining(
                         FileSystems.getDefault().getSeparator()
-                )) + "Impl.java");
+                )) + FileSystems.getDefault().getSeparator() + aClass.getSimpleName() + "Impl.java");
         try {
             Files.createDirectories(classPath.getParent());
-            Files.createFile(classPath);
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new ImplerException("Can't create parent directory: " + e.getMessage());
         }
         try (Writer writer = Files.newBufferedWriter(classPath)) {
             implClass(writer, aClass);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
-    private void implClass(Writer writer, Class<?> aClass) throws IOException {
+    private static void implClass(Writer writer, Class<?> aClass) throws IOException, ImplerException {
         write(writer, "package " + aClass.getPackageName() + ";");
-        write(writer, "public class " + aClass.getSimpleName() + "Impl " + (aClass.isInterface() ? "implements " : "extends ") + aClass.getSimpleName() + " {");
+        write(writer, "public class " + aClass.getSimpleName() + "Impl " + (aClass.isInterface() ? "implements " : "extends ") +
+                (aClass.getDeclaringClass() == null ? "" : aClass.getDeclaringClass().getSimpleName() + ".") + aClass.getSimpleName() + " {");
         implCons(writer, aClass);
         implMethods(writer, aClass);
         write(writer, "}");
     }
 
-    private void implCons(Writer writer, Class<?> aClass) throws IOException {
-        for (Constructor<?> c : aClass.getConstructors()) {
-            write(writer, "public " + aClass.getSimpleName() + "Impl(" + toArgs(c.getParameterTypes()) + ")" + " {");
-            write(writer, "super(" + toArgs(c.getParameterTypes(), false) + ");");
+    private static void implCons(Writer writer, Class<?> aClass) throws IOException, ImplerException {
+        Set<Constructor<?>> cons = Stream.concat(Arrays.stream(aClass.getConstructors()), Arrays.stream(aClass.getDeclaredConstructors())).collect(Collectors.toSet());
+        boolean hasCons = aClass.isInterface();
+        for (Constructor<?> c : cons) {
+            if (Modifier.isPrivate(c.getModifiers())) {
+                continue;
+            } else {
+                hasCons = true;
+            }
+            write(writer, "public " +
+                    aClass.getSimpleName() + "Impl("
+                    + getArgs(c.getParameterTypes()) + ")"
+                    + getExceptionTypes(c.getExceptionTypes()) + " {");
+            write(writer, "super(" + getArgs(c.getParameterTypes(), false) + ");");
             write(writer, "}");
+        }
+        if (!hasCons) {
+            throw new ImplerException(aClass.getSimpleName() + "hasn't accessible constructor");
         }
     }
 
-    private void implMethods(Writer writer, Class<?> aClass) throws IOException {
-        for (Method m : Stream.concat(Arrays.stream(aClass.getMethods()), Arrays.stream(aClass.getDeclaredMethods())).collect(Collectors.toSet())) {
-            int mods = m.getModifiers();
-            if (Modifier.isStatic(mods) || Modifier.isFinal(mods) || Modifier.isPrivate(mods)) {
+    private static void implMethods(Writer writer, Class<?> aClass) throws IOException, ImplerException {
+        for (Method m : getAllMethods(aClass)) {
+            if (m == null) {
                 continue;
             }
             write(writer, "@Override");
-            write(writer, "public " + getReturnType(m.getReturnType()) + " " + m.getName() + "(" + toArgs(m.getParameterTypes()) + ")" + " {");
+            write(writer, "public " +
+                    getReturnType(m.getReturnType()) + " " + m.getName()
+                    + "(" + getArgs(m.getParameterTypes()) + ")"
+                    + getExceptionTypes(m.getExceptionTypes()) + " {");
             write(writer, "return " + getZeroType(m.getReturnType()) + ";");
             write(writer, "}");
         }
     }
 
-    private String getZeroType(Class<?> returnType) {
+    private static Collection<Method> getAllMethods(Class<?> aClass) {
+        Map<String, Method> methods = new HashMap<>();
+        Set<String> notOverriden = new HashSet<>();
+        while (aClass != null) {
+            for (Method m : Stream.concat(Arrays.stream(aClass.getMethods()), Arrays.stream(aClass.getDeclaredMethods())).toList()) {
+                int mods = m.getModifiers();
+                if (Modifier.isStatic(mods) || Modifier.isFinal(mods) || Modifier.isPrivate(mods)) {
+                    notOverriden.add(m.getName() + Arrays.toString(m.getParameterTypes()));
+                } else if (Modifier.isAbstract(mods)) {
+                    methods.putIfAbsent(m.getName() + Arrays.toString(m.getParameterTypes()), m);
+                }
+            }
+            aClass = aClass.getSuperclass();
+        }
+        return methods
+                .entrySet()
+                .stream()
+                .filter(m -> !notOverriden.contains(m.getKey()))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toSet());
+    }
+
+    private static String getExceptionTypes(Class<?>[] exceptionTypes) {
+        return (exceptionTypes.length == 0 ? " " : " throws " + Arrays.stream(exceptionTypes).map(Class::getName).collect(Collectors.joining(", ")));
+    }
+
+    private static String getZeroType(Class<?> returnType) {
         if (!returnType.isPrimitive()) {
             return "null";
         }
@@ -84,7 +127,10 @@ public class Implementor implements Impler {
         return "0";
     }
 
-    private String getReturnType(Class<?> returnType) {
+    private static String getReturnType(Class<?> returnType) throws ImplerException {
+        if (Modifier.isPrivate(returnType.getModifiers())) {
+            throw new ImplerException("Private returnType in method: " + returnType.getSimpleName());
+        }
         if (returnType.isArray()) {
             return getReturnType(returnType.componentType()) + "[]";
         } else {
@@ -92,17 +138,22 @@ public class Implementor implements Impler {
         }
     }
 
-    private String toArgs(Class<?>[] parameterTypes) {
-        return toArgs(parameterTypes, true);
+    private static String getArgs(Class<?>[] parameterTypes) throws ImplerException {
+        return getArgs(parameterTypes, true);
     }
 
-    private String toArgs(Class<?>[] parameterTypes, boolean withTypes) {
+    private static String getArgs(Class<?>[] parameterTypes, boolean withTypes) throws ImplerException {
+        for (Class<?> p : parameterTypes) {
+            if (Modifier.isPrivate(p.getModifiers())) {
+                throw new ImplerException("Private arg in method: " + p.getSimpleName());
+            }
+        }
         return IntStream.range(0, parameterTypes.length)
                 .mapToObj(i -> (withTypes ? parameterTypes[i].getCanonicalName() + " " : "") + "arg" + i)
                 .collect(Collectors.joining(", "));
     }
 
-    private void write(Writer writer, String s) throws IOException {
+    private static void write(Writer writer, String s) throws IOException {
         writer.write(s + System.lineSeparator());
         System.err.println(s);
     }
