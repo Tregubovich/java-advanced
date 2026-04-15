55ddf6d8e6538ff169fc017b43fda1ec87de62fe
Tregubovich Andrei <326632andrey@gmail.com>
2026-04-15 17:59:37 +0300

    Homework 9

--- java-solutions/info/kgeorgiy/ja/tregubovich/iterative/ParallelMapperImpl.java
+++ java-solutions/info/kgeorgiy/ja/tregubovich/iterative/ParallelMapperImpl.java
@@ -4,9 +4,10 @@ import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class ParallelMapperImpl implements ParallelMapper {
-    private final Thread[] workers;
+    private final List<Thread> workers;
     private final Queue<Runnable> tasks;
 
     private volatile boolean closed = false;
@@ -16,52 +17,47 @@ public class ParallelMapperImpl implements ParallelMapper {
      *
      * @param threads number of threads for mapping
      */
-    public ParallelMapperImpl(int threads) {
-        workers = new Thread[threads];
+    public ParallelMapperImpl(final int threads) {
+        workers = new ArrayList<>(Collections.nCopies(threads, null));
         tasks = new LinkedList<>();
-        for (int t = 0; t < threads; t++) {
-            workers[t] = new Thread(() -> {
-                Runnable task;
-                while (!Thread.currentThread().isInterrupted()) {
-                    try {
-                        synchronized (tasks) {
-                            while (tasks.isEmpty() && !closed) {
-                                tasks.wait();
-                            }
-                            if (tasks.isEmpty()) {
-                                return;
-                            }
-                            task = tasks.poll();
+         IntStream.range(0, threads).forEach(t-> workers.set(t, new Thread(() -> {
+            Runnable task;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    synchronized (tasks) {
+                        while (tasks.isEmpty() && !closed) {
+                            tasks.wait();
                         }
-                        task.run();
-                    } catch (InterruptedException _) {
-                        break;
+                        if (tasks.isEmpty()) {
+                            return;
+                        }
+                        task = tasks.poll();
                     }
+                    task.run();
+                } catch (InterruptedException _) {
+                    break;
                 }
-            });
-            workers[t].start();
-        }
+            }
+        })));
+        workers.forEach(Thread::start);
     }
 
     private static class Counter {
         int remain;
 
-        Counter(int remain) {
+        Counter(final int remain) {
             this.remain = remain;
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public <T, R> List<R> map(Function<? super T, ? extends R> function, List<? extends T> list) throws InterruptedException {
-        int n = list.size();
-        List<R> res = new ArrayList<>(Collections.nCopies(n, null));
-        Counter cnt = new Counter(list.size());
+    public <T, R> List<R> map(final Function<? super T, ? extends R> function, final List<? extends T> list) throws InterruptedException {
+        final int n = list.size();
+        final List<R> res = new ArrayList<>(Collections.nCopies(n, null));
+        final Counter cnt = new Counter(list.size());
         final List<RuntimeException> ex = Collections.synchronizedList(new ArrayList<>());
         for (int i = 0; i < n; i++) {
-            int idx = i;
+            final int idx = i;
             synchronized (tasks) {
                 if (closed) {
                     throw new IllegalStateException("ParallelMapper was closed");
@@ -69,7 +65,7 @@ public class ParallelMapperImpl implements ParallelMapper {
                 tasks.add(() -> {
                     try {
                         res.set(idx, function.apply(list.get(idx)));
-                    } catch (RuntimeException exception) {
+                    } catch (final RuntimeException exception) {
                         ex.add(exception);
                     }
                     synchronized (cnt) {
@@ -87,7 +83,7 @@ public class ParallelMapperImpl implements ParallelMapper {
         }
         if (!ex.isEmpty()) {
             RuntimeException exception = null;
-            for (RuntimeException e : ex) {
+            for (final RuntimeException e : ex) {
                 if (exception != null) {
                     exception.addSuppressed(e);
                 } else {
@@ -102,22 +98,16 @@ public class ParallelMapperImpl implements ParallelMapper {
         return res;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void close() {
         closed = true;
-        synchronized (tasks) {
-            tasks.notifyAll();
-        }
-        for (Thread t : workers) {
+        for (final Thread t : workers) {
             t.interrupt();
         }
-        for (Thread t : workers) {
+        for (final Thread t : workers) {
             try {
                 t.join();
-            } catch (InterruptedException ignored) {
+            } catch (final InterruptedException ignored) {
             }
         }
     }
